6ee8784d99159051aa9e472cdcbf9257cc790cbf
thaonp <thaonpfx15559@funix.edu.vn>
2022-07-28 10:08:58 +0700

    store data in db & custom err handling

--- be/app.js
+++ be/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const feedRoutes = require('./routes/feed')
 
-const app = express() 
+const app = express()
 const port = 8080
+const connectionURL = 'mongodb+srv://test:bJYVI29LEAjl147U@cluster0.ti4jx.mongodb.net/message'
 
-app.use(express.json()) 
+app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader('access-control-allow-origin', '*'); // allow access from any domain 
@@ -16,4 +18,15 @@ app.use((req, res, next) => {
 
 app.use(feedRoutes)
 
-app.listen(port)
+app.use((err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500
+    const msg = err.message
+    res.status(status).json({ message: msg })
+})
+
+mongoose
+    .connect(connectionURL)
+    .then(() => app.listen(port))
+    .catch(err => console.log(err))
+
--- be/controllers/feed.js
+++ be/controllers/feed.js
@@ -1,5 +1,6 @@
-
 const { validationResult } = require('express-validator')
+
+const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({ // success
@@ -17,27 +18,35 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.postPost = (req, res, next) => {
-    const title = req.body.title
-    const content = req.body.content
 
-    const errors = validationResult(req) // validation results get from request
-
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'validation failed',
-            errors: errors.array()
-        })
+        const err = new Error('validation failed') // pass data to 'message' field of new err Obj
+        err.statusCode = 422
+        throw err // not wrap around in async code => auto exit the func and try to reach the next func
     }
 
-    res.status(201).json({ // sucessfully created
-        post: {
-            _id: Math.random(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'ABC'
-            },
-            createdAt: new Date()
-        }
+    const title = req.body.title
+    const content = req.body.content
+    const post = new Post({
+        title: title,
+        content: content,
+        creator: { name: 'ABC' },
+        imageUrl: 'https://as2.ftcdn.net/jpg/02/29/75/83/220_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg'
+
     })
+    post
+        .save()
+        .then(postData => {
+            res.status(201).json({ // sucessfully created
+                post: postData
+            })
+        })
+        .catch(err => { // if new post is failed in data validation
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err) // pass the err to the next err handling exp mw
+        });
+
 }
